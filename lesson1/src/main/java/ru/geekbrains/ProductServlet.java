b692f29cf2f25dfba8f468cb72da6cf4902baa49
Bananza93 <zubryaev.sergey@gmail.com>
2021-11-25 16:52:36 +0400

    Lesson 1 HW

--- lesson1/src/main/java/ru/geekbrains/ProductServlet.java
+++ lesson1/src/main/java/ru/geekbrains/ProductServlet.java
@@ -1,6 +1,5 @@
 package ru.geekbrains;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
