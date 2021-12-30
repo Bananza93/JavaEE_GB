0eb7d27dc6121bd67b79f485d2ee5856570a13d2
Bananza93 <zubryaev.sergey@gmail.com>
2021-12-30 19:29:33 +0400

    Lesson 10 HW

--- lesson9/src/main/java/ru/geekbrains/lesson9/Lesson9Application.java
+++ lesson9/src/main/java/ru/geekbrains/lesson9/Lesson9Application.java
@@ -2,14 +2,6 @@ package ru.geekbrains.lesson9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.annotation.SessionScope;
-import ru.geekbrains.lesson9.bean.Cart;
-import ru.geekbrains.lesson9.dto.ProductDto;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 
 @SpringBootApplication
 public class Lesson9Application {
@@ -17,5 +9,4 @@ public class Lesson9Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Lesson9Application.class, args);
 	}
-
 }
